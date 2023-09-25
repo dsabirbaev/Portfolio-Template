70323f5c4a0650230ab8080d3e05e374c55716f3
Davranbek <davran9559@gmail.com>
2023-09-25 22:08:22 +0500

    header desktop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,21 @@ const index = () => {
     return (
         <header>
             <div className="container">
-                <h1>Header</h1>
+                <nav className="nav">
+                    <a className="nav__logo" href="#">D.S</a>
+
+                    <div className="nav__link">
+                        <ul className="nav__list">
+                            <li className="nav__list--item"><a className="nav__list--item-link" href="#">About</a></li>
+                            <li className="nav__list--item"><a className="nav__list--item-link" href="#">Work</a></li>
+                            <li className="nav__list--item"><a className="nav__list--item-link" href="#">Testimonials</a></li>
+                        </ul>
+                        <div className="nav__btn">
+                            <i title="Switch to dark" className='bx bx-moon nav__btn--dark'></i>
+                            <a className="nav__btn--download" href="#">Download CV</a>
+                        </div>
+                    </div>
+                </nav>
             </div>
         </header>
     );
