b9c193cbb5c16d71ea103083827cfa66d66a02e4
Davranbek <davran9559@gmail.com>
2023-09-26 01:25:02 +0500

    add drop menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,18 +3,21 @@ import "./style.scss";
 
 const index = () => {
     const [menu, setMenu] = useState(false);
-
+    const [dark, setDark] = useState(false);
+    const navLinkStyle = menu ? { right: 0 } : {};
     return (
         <header>
             <div className="container">
                 <nav className="nav">
                     <a className="nav__logo" href="#">D.S</a>
-
-                    {
-                        menu ? <i onClick={() => setMenu(true)} className='bx bx-x close-menu'></i> : <i className='bx bx-menu menu'></i>  
-                    }
-                    
-                    <div className="nav__link">
+                    <span onClick={() => setMenu(!menu)} >
+                        {
+                            menu ?
+                                <i className='bx bx-x close-menu'></i>
+                                : <i className='bx bx-menu menu'></i>
+                        }
+                    </span>
+                    <div className="nav__link" style={navLinkStyle}>
                         <ul className="nav__list">
                             <li className="nav__list--item"><a className="nav__list--item-link" href="#">About</a></li>
                             <li className="nav__list--item"><a className="nav__list--item-link" href="#">Work</a></li>
@@ -25,7 +28,14 @@ const index = () => {
                         <div className="nav__btn">
                             <div className="nav__btn--list">
                                 <span className="nav__btn--text">Switch Theme</span>
-                                <i title="Switch to dark" className='bx bx-moon nav__btn--dark'></i>
+                                <span onClick={() => setDark(!dark)}>
+                                    {
+                                        dark ? <i title="Switch to light" className='bx bx-sun nav__btn--dark'></i> :
+                                        <i title="Switch to dark" className='bx bx-moon nav__btn--dark'></i>
+                                        
+                                    }
+                                </span>
+                                
                             </div>
 
                             <a className="nav__btn--download" href="#">Download CV</a>
