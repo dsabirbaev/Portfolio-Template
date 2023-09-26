6d3aeca912b2e868f77a78e6427bb9fb9ec63087
Davranbek <davran9559@gmail.com>
2023-09-26 12:16:12 +0500

    add dark func

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,44 +1,65 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./style.scss";
 
 const index = () => {
     const [menu, setMenu] = useState(false);
-    const [dark, setDark] = useState(false);
+    const [dark, setDark] = useState(() => {
+        const storedDarkMode = localStorage.getItem('darkMode');
+        return storedDarkMode ? false : true;
+
+    });
     const navLinkStyle = menu ? { right: 0 } : {};
+
+    const toggleDarkMode = () => {
+       
+        setDark(!dark);
+        // document.documentElement.classList.toggle("dark");
+
+    }
+    useEffect(() => {
+       
+        localStorage.setItem('darkMode', JSON.stringify(dark));
+        if (dark === true) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [dark]);
+
     return (
-        <header>
+        <header className="dark:bg-[#030712]">
             <div className="container">
                 <nav className="nav">
-                    <a className="nav__logo" href="#">D.S</a>
-                    <span onClick={() => setMenu(!menu)} >
+                    <a className="nav__logo dark:text-white" href="#">D.S</a>
+                    <span className="dark:text-white" onClick={() => setMenu(!menu)} >
                         {
                             menu ?
                                 <i className='bx bx-x close-menu'></i>
                                 : <i className='bx bx-menu menu'></i>
                         }
                     </span>
-                    <div className="nav__link" style={navLinkStyle}>
-                        <ul className="nav__list">
+                    <div className="nav__link dark:bg-[#030712]" style={navLinkStyle}>
+                        <ul className="nav__list dark:text-white">
                             <li className="nav__list--item"><a className="nav__list--item-link" href="#">About</a></li>
                             <li className="nav__list--item"><a className="nav__list--item-link" href="#">Work</a></li>
                             <li className="nav__list--item"><a className="nav__list--item-link" href="#">Testimonials</a></li>
                             <li className="nav__list--item"><a className="nav__list--item-link" href="#">Contact</a></li>
                         </ul>
                         <span className="nav__line">|</span>
-                        <div className="nav__btn">
+                        <div className="nav__btn dark:text-white">
                             <div className="nav__btn--list">
                                 <span className="nav__btn--text">Switch Theme</span>
-                                <span onClick={() => setDark(!dark)}>
+                                <span onClick={toggleDarkMode}>
                                     {
                                         dark ? <i title="Switch to light" className='bx bx-sun nav__btn--dark'></i> :
-                                        <i title="Switch to dark" className='bx bx-moon nav__btn--dark'></i>
-                                        
+                                            <i title="Switch to dark" className='bx bx-moon nav__btn--dark'></i>
+
                                     }
                                 </span>
-                                
+
                             </div>
 
-                            <a className="nav__btn--download" href="#">Download CV</a>
+                            <a className="nav__btn--download dark:bg-gray-700 dark:hover:text-white" href="#">Download CV</a>
                         </div>
                     </div>
                 </nav>
--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -5,19 +5,19 @@ import "./style.scss";
 
 const index = () => {
     return (
-        <section className='intro'>
+        <section className='intro dark:bg-[#030712]'>
             <div className='container'>
                 <div className="intro__inner">
                     <div className="intro__right">
-                        <h1 className="intro__right--title">Hi, I'm Davranbek 👋</h1>
-                        <p className="intro__right--text">I'm a full stack developer (React.js & Vue.js and Node.js & Laravel ) with a focus on creating  exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even though I have been creating web applications for over 7 years, I still love it as if it was something new.</p>
-                        <div className="intro__right--location">
+                        <h1 className="intro__right--title dark:text-white">Hi, I'm Davranbek 👋</h1>
+                        <p className="intro__right--text dark:text-white">I'm a full stack developer (React.js & Vue.js and Node.js & Laravel ) with a focus on creating  exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even though I have been creating web applications for over 7 years, I still love it as if it was something new.</p>
+                        <div className="intro__right--location dark:text-white">
                              <i className='bx bx-map'></i> <p>Uzbekistan, Tashkent</p>
                         </div>
                         <div className="intro__right--status">
-                            <i className='bx bxs-circle'></i> <p>Available for new projects</p>
+                            <i className='bx bxs-circle'></i> <p className="dark:text-white">Available for new projects</p>
                         </div>
-                        <div className="intro__right--socials">
+                        <div className="intro__right--socials dark:text-white">
                             <a href="#"><i className='bx bxl-github github'></i></a>
                             <a href="#"><i className='bx bxl-twitter twitter'></i></a>
                             <a href="#"><i className='bx bxl-figma figma'></i></a>
